67874576d175efe9e2d0a61c9164949ad7320dd2
lap1 <lap1>
2019-06-03 15:12:59 +0800

    test

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 const express = require("express")
 const app = express()
-// const port = process.env.PORT || 5000 // win测试
-const port = 3000 // 阿里云ECS服务器
-const fs = require("fs")
+const port = process.env.PORT || 5000 // win测试
+// const port = 3000 // 阿里云ECS服务器
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
-
-// 转换https服务
-const https = require("https")
-var options = {
-	key: fs.readFileSync('./static/2301701_www.sushen1990.cn.key'),
-	cert: fs.readFileSync('./static/2301701_www.sushen1990.cn.pem')
-}
-
 // 监听服务器
-// app.listen(port, ()=> {
-// 	console.log(`服务器运行在端口[${port}]`)
-// })
-// 
-https.createServer(options,app).listen(port)
-console.log(`https服务器运行在端口[${port}]`)
+app.listen(port, ()=> {
+	console.log(`服务器运行在端口[${port}]`)
+})
+
+
 // 访问页面内容
 app.get("/",(req,res) =>{
 	res.send("hello q")
@@ -30,7 +20,7 @@ app.get("/",(req,res) =>{
 // 访问数据库
 // mongoRUI = "mongodb://aly_root:d456_FJ35LLL@127.0.0.1:27899/admin"
 // mongoRUI = "mongodb://aly_root:d456_FJ35LLL@39.97.33.102:27899/admin"
-mongoRUI="mongodb://localhost/admin"
+mongoRUI="mongodb://localhost/db1"
 const DB =  mongoRUI
 mongoose.connect(DB,{ useNewUrlParser: true })
         .then(() => console.log("数据库连接成功"))
@@ -45,3 +35,6 @@ const usersAPI = require("./router/api/users")
 app.use("/api/users",usersAPI) 
 
 
+// 错误的innerHTML
+const innerHtmlApi = require("./router/api/innerHtml")
+app.use("/api/innerHtml",innerHtmlApi) 
