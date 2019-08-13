2e83dfc40efdb9861f7d32686e363313ce5a0be6
lap1 <lap1>
2019-08-13 17:54:54 +0800

    学生是否付款

--- router/api/class.js
+++ router/api/class.js
@@ -1,9 +1,9 @@
 'use strict';
 const express = require("express");
 const router = express.Router();
-const classDB = require("../../models/classModel.js")
-const schoolDB = require("../../models/schoolModel.js")
-const config = require("../../config.js")
+const classDB = require("../../models/classModel.js");
+const schoolDB = require("../../models/schoolModel.js");
+const config = require("../../config.js");
 const Helper = require('../../common/helper');
 
 // 测试接口是否连通 start ↓
@@ -32,38 +32,37 @@ router.post("/classSave", (req, res) => {
 			msg: "no",
 			data: "Scode错误"
 		})
-	}
+	};
 	if (Helper.checkReal(schoolId)) {
 		return res.status(400).json({
 			msg: "no",
 			data: "缺少参数schoolId"
 		})
-	}
+	};
 	if (Helper.checkReal(schoolName)) {
 		return res.status(400).json({
 			msg: "no",
 			data: "缺少参数schoolName"
 		})
-	}
+	};
 	if (Helper.checkReal(grade)) {
 		return res.status(400).json({
 			msg: "no",
 			data: "缺少参数grade"
 		})
-	}
+	};
 	if (Helper.checkReal(_class)) {
 		return res.status(400).json({
 			msg: "no",
 			data: "缺少参数_class"
 		})
-	}
+	};
 	if (schoolId.length != 24) {
 		return res.status(400).json({
 			msg: "no",
 			data: "schoolID长度应为24位"
 		})
-	}
-
+	};
 	// 参数验证 end   ↑
 
 	let whereStr = {
@@ -71,7 +70,7 @@ router.post("/classSave", (req, res) => {
 		grade: grade,
 		_class: _class,
 		isShow: true
-	}
+	};
 
 	classDB.findClassByStr(
 		whereStr,
--- router/api/veryfiCode.js
+++ router/api/veryfiCode.js
@@ -5,86 +5,104 @@ const request = require('request');
 const soap = require('soap');
 const crypto = require('crypto');
 const Helper = require('../../common/helper');
-const config = require("../../config")
-const veryfiCodeDB = require("../../models/veryfiCodeModel.js")
+const config = require("../../config");
+const veryfiCodeDB = require("../../models/veryfiCodeModel.js");
 
 const Core = require('@alicloud/pop-core');
 
-router.get("/test",(req,res) =>{ 
-    res.json({msg:"hello VeryfiCode"})
+router.get("/test", (req, res) => {
+	res.json({
+		msg: "hello VeryfiCode"
+	})
 })
 
- //  用户获取验证码
-router.post("/sendVeryfiCode", (req,res)=>{
-	 let mobile = req.body.mobile ;
-	 let modelId = req.body.modelId ;
-	 if (Helper.checkReal(mobile)) {
-	     return res.status(400).json({msg: "手机号码不能为空！", data:null})
-	 } ;
-	 	if (Helper.checkReal(modelId)) {
-	     return res.status(400).json({msg: "关键值不能为空！", data:null})
-	 } ;
-	 
-	let veryfiCode = '' ;
-	veryfiCode = Helper.int6();
+//  用户获取验证码
+router.post("/sendVeryfiCode", (req, res) => {
 	
-	var client = new Core({
-	  accessKeyId: config.accessKeyId,
-	  accessKeySecret: config.accessKeySecret,
-	  endpoint: 'https://dysmsapi.aliyuncs.com',
-	  apiVersion: '2017-05-25'
-	});
+	let mobile = req.body.mobile;
+	let Scode = req.body.Scode;
 
-	var params = {
-	  "PhoneNumbers": mobile,
-	  "SignName": "信天游",
-	  "TemplateCode": "SMS_163480790",
-	  "TemplateParam": '"{\"code\":\"'+veryfiCode+'\"}"'
-	}
-
-	var requestOption = {
-	  method: 'POST'
+	// 参数验证 start ↓
+	if (Helper.checkReal(mobile)) {
+		return res.status(400).json({
+			msg: "no",
+			data: "手机号码不能为空"
+		})
 	};
-	veryfiCodeDB.add(veryfiCode, mobile, modelId, function(err,result){
-		if(err){
-			return res.status(500).json({msg: "后端API错误！", data:err})
-		}	
-		res.json(
-			{msg: "ok", data:result}
-		)
+	if (Helper.checkReal(Scode) || Scode != config.Scode) {
+		return res.status(400).json({
+			msg: "no",
+			data: "Scode错误"
+		})
+	};
+	// 参数验证 end   ↑
 
-	})
 
-	// client.request('SendSms', params, requestOption).then((result) => {
-	//   console.log(JSON.stringify(result));
-	// }, (ex) => {
-	//   console.log(ex);
-	// })
-	 
+	let veryfiCode = '';
+	veryfiCode = Helper.int6();
 
- })
+	var client = new Core({
+		accessKeyId: config.accessKeyId,
+		accessKeySecret: config.accessKeySecret,
+		endpoint: 'https://dysmsapi.aliyuncs.com',
+		apiVersion: '2017-05-25'
+	});
+	var params = {
+		"PhoneNumbers": mobile,
+		"SignName": "信天游",
+		"TemplateCode": "SMS_163480790",
+		"TemplateParam": '"{\"code\":\"' + veryfiCode + '\"}"'
+	}
+	var requestOption = {
+		method: 'POST'
+	};
+	veryfiCodeDB.add(veryfiCode, mobile, modelId, function(err, result) {
+		if (err) {
+			return res.status(500).json({
+				msg: "后端API错误！",
+				data: err
+			})
+		}
+		res.json({
+			msg: "ok",
+			data: result
+		})
+	});
+})
 
 
 // 登录的时候验证验证码
-router.post("/checkVeryfiCode", (req,res) =>{
+router.post("/checkVeryfiCode", (req, res) => {
 	let mobile = req.body.mobile;
 	let modelId = req.body.modelId;
 	let veryfiCode = req.body.veryfiCode;
 	if (Helper.checkReal(mobile)) {
-		return res.status(400).json({msg: "手机号码不能为空！", data:null})
+		return res.status(400).json({
+			msg: "手机号码不能为空！",
+			data: null
+		})
 	};
 	if (Helper.checkTel(mobile)) {
-		return res.status(400).json({msg: "手机号码格式不正确！", data:null})
-	};		
+		return res.status(400).json({
+			msg: "手机号码格式不正确！",
+			data: null
+		})
+	};
 	if (Helper.checkReal(modelId)) {
-		return res.status(400).json({msg: "关键值不能为空！", data:null})
+		return res.status(400).json({
+			msg: "关键值不能为空！",
+			data: null
+		})
 	};
 	if (Helper.checkReal(veryfiCode)) {
-		return res.status(400).json({msg: "验证码不能为空！", data:null})
+		return res.status(400).json({
+			msg: "验证码不能为空！",
+			data: null
+		})
 	};
 
 	// 检查验证码
-	veryfiCodeDB.findCodeByMobile(mobile, modelId, function (err, result) {
+	veryfiCodeDB.findCodeByMobile(mobile, modelId, function(err, result) {
 		if (err) {
 			return res.status(500).json({
 				msg: "后端API错误",
@@ -103,17 +121,18 @@ router.post("/checkVeryfiCode", (req,res) =>{
 				data: result
 			})
 		};
-		var nowTime = new Date().getTime();		
+		var nowTime = new Date().getTime();
 		if (result.code == veryfiCode && result.time < nowTime) {
 			return res.status(404).json({
 				msg: "验证码失效，请重新获取验证码！",
 				data: result
 			})
 		};
-		res.json(
-			{msg: "ok", data:result}
-		)
-		
+		res.json({
+			msg: "ok",
+			data: result
+		})
+
 	})
 
 
